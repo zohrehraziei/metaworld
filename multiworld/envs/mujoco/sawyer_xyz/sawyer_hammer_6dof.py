cca6a62dd59d0b9b79d530cbfcce54bb95dae787
zhanpenghe <zhanpenh@usc.edu>
2019-06-23 16:06:37 -0700

    Fix hammering

--- multiworld/envs/mujoco/sawyer_xyz/sawyer_hammer_6dof.py
+++ multiworld/envs/mujoco/sawyer_xyz/sawyer_hammer_6dof.py
@@ -124,7 +124,6 @@ class SawyerHammer6DOFEnv(SawyerXYZEnv):
         self.viewer.cam.trackbodyid = -1
 
     def step(self, action):
-        self.render()
         # self.set_xyz_action_rot(action[:7])
         if self.rotMode == 'euler':
             action_ = np.zeros(7)
