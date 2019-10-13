0e55f1f225be13789747b38194ef280143ff0994
zhanpenghe <zhanpeng@cs.columbia.edu>
2019-10-12 22:51:28 -0400

    Update SawyerPushBackEnv

--- metaworld/envs/mujoco/sawyer_xyz/sawyer_push_back.py
+++ metaworld/envs/mujoco/sawyer_xyz/sawyer_push_back.py
@@ -12,6 +12,7 @@ from pyquaternion import Quaternion
 from metaworld.envs.mujoco.utils.rotation import euler2quat
 from metaworld.envs.mujoco.sawyer_xyz.base import OBS_TYPE
 
+
 class SawyerPushBackEnv(SawyerXYZEnv):
     def __init__(
             self,
@@ -137,7 +138,7 @@ class SawyerPushBackEnv(SawyerXYZEnv):
 
     def _get_obs(self):
         hand = self.get_endeff_pos()
-        objPos =  self.data.get_geom_xpos('objGeom')
+        objPos =  self.data.get_geom_xpos('obj_geom')
         flat_obs = np.concatenate((hand, objPos))
         if self.obs_type == 'with_goal_and_id':
             return np.concatenate([
@@ -157,7 +158,7 @@ class SawyerPushBackEnv(SawyerXYZEnv):
 
     def _get_obs_dict(self):
         hand = self.get_endeff_pos()
-        objPos =  self.data.get_geom_xpos('objGeom')
+        objPos =  self.data.get_geom_xpos('obj_geom')
         flat_obs = np.concatenate((hand, objPos))
         return dict(
             state_observation=flat_obs,
@@ -182,7 +183,7 @@ class SawyerPushBackEnv(SawyerXYZEnv):
         This should be use ONLY for visualization. Use self._state_goal for
         logging, learning, etc.
         """
-        objPos =  self.data.get_geom_xpos('objGeom')
+        objPos =  self.data.get_geom_xpos('obj_geom')
         self.data.site_xpos[self.model.site_name2id('objSite')] = (
             objPos
         )
@@ -208,18 +209,18 @@ class SawyerPushBackEnv(SawyerXYZEnv):
     def adjust_initObjPos(self, orig_init_pos):
         #This is to account for meshes for the geom and object are not aligned
         #If this is not done, the object could be initialized in an extreme position
-        diff = self.get_body_com('obj')[:2] - self.data.get_geom_xpos('objGeom')[:2]
+        diff = self.get_body_com('obj')[:2] - self.data.get_geom_xpos('obj_geom')[:2]
         adjustedPos = orig_init_pos[:2] + diff
 
         #The convention we follow is that body_com[2] is always 0, and geom_pos[2] is the object height
-        return [adjustedPos[0], adjustedPos[1],self.data.get_geom_xpos('objGeom')[-1]]
+        return [adjustedPos[0], adjustedPos[1],self.data.get_geom_xpos('obj_geom')[-1]]
 
     def reset_model(self):
         self._reset_hand()
         self._state_goal = self.goal.copy()
         self.obj_init_pos = self.adjust_initObjPos(self.init_config['obj_init_pos'])
         self.obj_init_angle = self.init_config['obj_init_angle']
-        self.objHeight = self.data.get_geom_xpos('objGeom')[2]
+        self.objHeight = self.data.get_geom_xpos('obj_geom')[2]
         if self.random_init:
             goal_pos = np.random.uniform(
                 self.obj_and_goal_space.low,
