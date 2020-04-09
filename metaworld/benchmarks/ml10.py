07d9655b7f0306e10087b1815f152171bbc39331
Keren Zhu <naeioi@hotmail.com>
2020-04-08 23:46:36 -0700

    Subset wrapper using from_tasks

--- metaworld/benchmarks/ml10.py
+++ metaworld/benchmarks/ml10.py
@@ -6,15 +6,16 @@ from metaworld.envs.mujoco.env_dict import MEDIUM_MODE_ARGS_KWARGS, MEDIUM_MODE_
 
 class ML10(MultiClassMultiTaskEnv, Benchmark):
 
-    def __init__(self, env_type='train', sample_all=False, task_name=None):
+    def __init__(self, env_type='train', sample_all=False, task_names=None):
         assert env_type == 'train' or env_type == 'test'
 
-        if task_name is not None:
-            if task_name not in MEDIUM_MODE_CLS_DICT[env_type]:
-                raise ValueError("{} does not exist in ML10 {} tasks".format(
-                    task_name, env_type))
-            cls_dict = {task_name: MEDIUM_MODE_CLS_DICT[env_type][task_name]}
-            args_kwargs = {task_name: MEDIUM_MODE_ARGS_KWARGS[env_type][task_name]}
+        if task_names is not None:
+            for task_name in task_names:
+                if task_name not in MEDIUM_MODE_CLS_DICT[env_type]:
+                    raise ValueError("{} does not exist in ML10 {} tasks".format(
+                        task_name, env_type))
+            cls_dict = {task_name: MEDIUM_MODE_CLS_DICT[env_type][task_name] for task_name in task_names}
+            args_kwargs = {task_name: MEDIUM_MODE_ARGS_KWARGS[env_type][task_name] for task_name in task_names}
         else:
             cls_dict = MEDIUM_MODE_CLS_DICT[env_type]
             args_kwargs = MEDIUM_MODE_ARGS_KWARGS[env_type]
@@ -28,9 +29,16 @@ class ML10(MultiClassMultiTaskEnv, Benchmark):
 
     @classmethod
     def from_task(cls, task_name):
-        if task_name in MEDIUM_MODE_CLS_DICT['train']:
-            return cls(env_type='train', sample_all=True, task_name=task_name)
-        elif task_name in MEDIUM_MODE_CLS_DICT['test']:
-            return cls(env_type='test', sample_all=True, task_name=task_name)
+        return cls.from_tasks([task_name])
+            
+    @classmethod
+    def from_tasks(cls, task_names):
+        assert task_names
+        
+        if task_names[0] in MEDIUM_MODE_CLS_DICT['train']:
+            return cls(env_type='train', sample_all=True, task_names=task_names)
+        elif task_names[0] in MEDIUM_MODE_CLS_DICT['test']:
+            return cls(env_type='test', sample_all=True, task_names=task_names)
         else:
-            raise ValueError('{} does not exist in ML10'.format(task_name))
+            raise ValueError('{} does not exist in ML10'.format(task_name[0]))
+            
